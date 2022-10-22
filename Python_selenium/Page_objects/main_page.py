783aa8232a2aedeb3d314f41541abeacf845acec
Ondra M <ondrej.malinka@tieto.com>
2022-10-23 00:33:01 +0200

    Added elements.

--- Python_selenium/Page_objects/main_page.py
+++ Python_selenium/Page_objects/main_page.py
@@ -4,6 +4,8 @@ from Page_objects.base_page import BasePage
 
 class MainPage(BasePage):
     # Identification of objects on main page.
+    category_section_header = (By.XPATH, "//div[@class='categoryPage']/h1")
+    item_detail_page = (By.XPATH, "//div[contains(@class, 'detail-page')]")
     computers_notebooks_menu_item = (By.LINK_TEXT, "Počítače a notebooky")
     computers_tile = (By.LINK_TEXT, "Počítače")
     first_computer_name = (By.XPATH, "//div[contains(@class, 'first firstRow')]//a[contains(@class, 'name')]")
@@ -29,15 +31,18 @@ class MainPage(BasePage):
     # Actions on main page.
     def main_page_hover_click_computers_notebooks_menu_item(self):
         self.base_hover_click(self.computers_notebooks_menu_item)
+        self.base_is_visible(self.category_section_header)
 
     def main_page_hover_click_pet_supplies_menu_item(self):
         self.base_hover_click(self.pet_supplies_menu_item)
+        self.base_is_visible(self.category_section_header)
 
     def main_page_click_computers_tile(self):
         self.base_click(self.computers_tile)
 
     def main_page_click_first_pet_suppy_item(self):
         self.base_click(self.first_pet_supply_item_link)
+        self.base_is_visible(self.item_detail_page)
 
     def main_page_click_watch_price(self):
         self.base_click(self.watchdog_link)
